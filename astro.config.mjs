fdd0e4e56c468cb88cb9548ac62c17c63bd8049b
Andre <andre8tr@gmail.com>
2025-09-17 20:12:47 -0600

    glass navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import path from 'path'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -14,4 +16,6 @@ export default defineConfig({
       },
     },
   },
+
+  integrations: [react()],
 })
